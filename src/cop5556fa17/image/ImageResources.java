34bc04035dd329b656182c6781c1337a6e2bd22a
vishalkg <vishal.gupta4081@gmail.com>
2017-12-07 20:25:44 -0500

    updated

--- src/cop5556fa17/image/ImageResources.java
+++ src/cop5556fa17/image/ImageResources.java
@@ -3,8 +3,9 @@ package cop5556fa17.image;
 public interface ImageResources {
 
 	public final static String imageFile1 = "/Users/vishalgupta/Acads/PLP/plp-project-f17/src/cop5556fa17/image/input_image.jpg";
+	//public final static String imageFile1 = "/cise/homes/vkg/Acads/plp/cop5556fa17/image/input_image.jpg\";
 	public final static String imageFile2 = "/Users/vishalgupta/Acads/PLP/plp-project-f17/src/cop5556fa17/image/output_image.jpg";
-	
+	//public final static String imageFile2 = "/cise/homes/vkg/Acads/plp/cop5556fa17/image/output_image.jpg\";
 	public final static String imageURL1 = "file://\\" + imageFile1;
 	
 	public final static int RED = 0xFF0000;
